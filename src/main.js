72bc19174b7100aed5d8f2e70a12d8d0ef608e21
onekuang <651776858@qq.com>
2018-07-07 09:13:34 +0800

    add es2015

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 
 var $ = window.jQuery = require("jquery")  
 require('bootstrap');
-import './common/bs/css/bootstrap.css'
 
-import './css/test.css'
-import './less/test.less'
-// const test = require('./js/project.js')
-import test from './js/project.js'
-console.log(12435);
-console.log(test);
+import './main.less'
+
+// import test from './js/project.js'
+// console.log(test);
 
 
-import {kk} from './js/pro.js'
-let i = 'name';
-let t = kk(i);
+// import {kk} from './js/pro.js'
+// let i = 'name';
+// let t = kk(i);
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Ex = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 module.exports = {
 	// 模式设置
@@ -28,6 +29,7 @@ module.exports = {
 
 		// 上线的时候,可以给地址添加前缀
 		publicPath: "http://localhost:8086/"
+		// publicPath: "./"
 	},
 
 
@@ -53,21 +55,21 @@ module.exports = {
 			{
 				test: /\.js$/,
 				loader: "babel-loader",
-				query:{
-					presets: ["env"]
-				},
+				
 				// 以下目录不处理
-				exclude: /node_modules/,
+				exclude: path.resolve(__dirname, 'node_modules'),
 				// 只处理以下目录
-				include: /src/,
+				include: path.resolve(__dirname, 'src'),
+				query:{
+					presets: ["env","es2015"]
+				},
 			},
    		{
         test: /(\.css|\.less)$/,
         use: Ex.extract({
         	fallback: "style-loader",
-        	use: "css-loader!less-loader!postcss-loader"
+        	use: "css-loader?importLoaders=1!postcss-loader!less-loader"
         })
-        // loader: Ex.extract("style-loader!css-loader!less-loader!postcss-loader")
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -85,13 +87,15 @@ module.exports = {
       },
       {
       	test: /\.(html|htm)$/i,
-      	loader: "html-withimg-loader",
+      	loader: "html-withimg-loader?min=false",
       }
 		]
 	},
 
 	// 插件
 	plugins: [
+	  // 每次打包清除/dist
+		new CleanWebpackPlugin(['dist/*.*']),
 		// 复制插件
 		new CopyWebpackPlugin([{
 				// 定义要拷贝的源目录
@@ -99,8 +103,13 @@ module.exports = {
 		    // 定义要拷贝到的目标目录
 		    to : __dirname + '/dist/static'
 		}]),
+		// 全局jquery
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+		}),
 		// 分离js css
-		new Ex("css/[name].css"),
+		new Ex("[name].css"),
 
 	  new webpack.BannerPlugin('版权所有，翻版必究'),
 	  // 热加载
@@ -116,7 +125,7 @@ module.exports = {
 			title: "首页",
 			filename: 	'index.html', 
 			// 模版位置
-			template: 'html-withimg-loader!' + __dirname + "/index.htm",  
+			template: 'html-withimg-loader!' + __dirname + "/view/index.htm",  
 			// script标签的位置
 			inject: 		'true',
 			// 指定引用的js, 默认引入所有打包的js
@@ -135,15 +144,15 @@ module.exports = {
 			},
 		}),
 
-		// new htmlWebpackPlugin({
-		// 	title: "列表",
-		// 	filename: 	'list.html', 
-		// 	template: __dirname + "/view/list/list.htm",  
-		// 	// 指定引用的js, 默认引入所有打包的js
-		// 	chunks: [
-		// 		'main'
-		// 	],
-		// }),
+		new htmlWebpackPlugin({
+			title: "产品列表",
+			filename: 	'product.html', 
+			template: 'html-withimg-loader!' +  __dirname + "/view/product.htm",  
+			// 指定引用的js, 默认引入所有打包的js
+			chunks: [
+				'main'
+			],
+		}),
 
 	],
 }
